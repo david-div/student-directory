9d887314ac1e10f7b44c1a3ace1ddf9cd672ea5e
David Divilly <d.divilly@gmail.com>
2017-07-19 16:23:18 +0000

    8.5 addtional student info

--- directory.rb
+++ directory.rb
@@ -4,13 +4,20 @@ def input_students
     # create an empty array
     students = []
     # get the first name
-    name = gets.chomp
     # while the name is not emplty, repeat this code
+    name = gets.chomp
     while !name.empty? do # as they have to press enter twice to stop it
+        puts "Have any hobbies?"
+        hobbies = gets.chomp
+        puts "Country of birth?"
+        country = gets.chomp
+        puts "Height?"
+        height  = gets.chomp
         # add the students hash to the array
-        students << {name: name, cohort: :november}  # adding the hash to the array, with nov cohort
+        students << {name: name, cohort: :november, hobbies: hobbies, country: country, height: height}  # adding the hash to the array, with nov cohort
         puts "Now we have #{students.count} students" # count on the array
-        # get anoter name from the yser
+        # get another name from the user
+        puts "Next name please:"
         name = gets.chomp
     end
     # return the array of students
@@ -26,6 +33,8 @@ def print(students)
     counter = 0
     until counter == students.size
     puts "#{counter + 1}. #{students[counter][:name]} (#{students[counter][:cohort]} cohort)"
+    puts "Addtional info:"
+    puts "Hobbies: #{students[counter][:hobbies]}, Country: #{students[counter][:country]}, and height: #{students[counter][:height]}"
     counter += 1
     end
 end
