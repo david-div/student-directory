4136582ef4bedcc13b58011e774c9e8473a1b50a
David Divilly <d.divilly@gmail.com>
2017-07-21 12:55:55 +0000

    9 added interactive menu

--- directory.rb
+++ directory.rb
@@ -9,7 +9,7 @@ def input_students
     while !name.empty? do # as they have to press enter twice to stop it
         puts "What cohort are they in?"
         cohort = gets.gsub(/\n/,"")
-        cohort = 'november'if cohort == ""
+        cohort = 'November'if cohort == ""
         puts "Have any hobbies?"
         hobbies = gets.gsub(/\n/,"")
         puts "Country of birth?"
@@ -17,7 +17,7 @@ def input_students
         puts "Height?"
         height  = gets.gsub(/\n/,"")
         # add the students hash to the array
-        students << {name: name, cohort: cohort.to_sym, hobbies: hobbies, country: country, height: height}  # adding the hash to the array, with nov cohort
+        students << {name: name, cohort: cohort.capitalize.to_sym, hobbies: hobbies, country: country, height: height}  # adding the hash to the array, with nov cohort
         puts "Now we have #{students.count} #{students.count == 1 ? "student" : "students"}" # count on the array
         # get another name from the user
         puts "Next name please:"
@@ -40,7 +40,7 @@ def print(students)
     until counter == students.size
     puts "#{counter + 1}. #{students[counter][:name]} (#{students[counter][:cohort]} cohort)".center(100)
     puts "Addtional info:".center(100)
-    puts "Hobbies: #{students[counter][:hobbies]}, Country of birth: #{students[counter][:country]}, height: #{students[counter][:height]}".center(100)
+    puts "Hobbies: #{students[counter][:hobbies]}, Country of birth: #{students[counter][:country]}, Height: #{students[counter][:height]}".center(100)
     counter += 1
     end
     puts "Students by cohort: ".center(100)
@@ -62,10 +62,28 @@ def print_footer(students)
 end
 end
 
-
+def interactive_menu
+    students = []
+    loop do
+        # 1. print the menu and ask for the user what to do
+        puts "1. Input the students"
+        puts "2. Show the students"
+        puts "9. Exit"
+        # 2. read the input and save it into a variable
+        selection = gets.chomp
+        # 3. do what the user has asked    
+        case selection
+        when "1" then  students = input_students # calling the method input students
+        when "2"
+            print_header
+            print(students)
+            print_footer(students)
+        when "9" then  exit  # this will cause the program to terminate
+        else           puts "I don't know what you meant, try again"
+        end
+    
+    end
+end
 # nothing happens until we call the methods
 
-students = input_students    # calling the method input_students and putting the results in sudents
-print_header
-print(students)
-print_footer(students)
+interactive_menu
