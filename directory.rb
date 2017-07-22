47909d40bf31f1add148fb30c0b31b7e5b535a0f
David Divilly <d.divilly@gmail.com>
2017-07-22 19:22:56 +0000

    10 refactoring

--- directory.rb
+++ directory.rb
@@ -1,8 +1,10 @@
+@students = [] # an empty array accessible to all methods
+
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
     # create an empty array
-    students = []
+    # students = []  # no longer needed as @students
     # get the first name
     # while the name is not emplty, repeat this code
     name = gets.chomp
@@ -17,14 +19,12 @@ def input_students
         puts "Height?"
         height  = gets.gsub(/\n/,"")
         # add the students hash to the array
-        students << {name: name, cohort: cohort.capitalize.to_sym, hobbies: hobbies, country: country, height: height}  # adding the hash to the array, with nov cohort
-        puts "Now we have #{students.count} #{students.count == 1 ? "student" : "students"}" # count on the array
+        @students << {name: name, cohort: cohort.capitalize.to_sym, hobbies: hobbies, country: country, height: height}  # adding the hash to the array, with nov cohort
+        puts "Now we have #{@students.count} #{@students.count == 1 ? "student" : "students"}" # count on the array
         # get another name from the user
         puts "Next name please:"
         name = gets.gsub(/\n/,"")
     end
-    # return the array of students
-    students
 end
 
 def print_header
@@ -32,20 +32,20 @@ def print_header
     puts "-----------".center(100)
 end
   
-def print(students)
+def print_students_list(students)
     
-    if students.size > 0
+    if @students.size > 0
     
     counter = 0
-    until counter == students.size
-    puts "#{counter + 1}. #{students[counter][:name]} (#{students[counter][:cohort]} cohort)".center(100)
+    until counter == @students.size
+    puts "#{counter + 1}. #{@students[counter][:name]} (#{@students[counter][:cohort]} cohort)".center(100)
     puts "Addtional info:".center(100)
-    puts "Hobbies: #{students[counter][:hobbies]}, Country of birth: #{students[counter][:country]}, Height: #{students[counter][:height]}".center(100)
+    puts "Hobbies: #{@students[counter][:hobbies]}, Country of birth: #{@students[counter][:country]}, Height: #{@students[counter][:height]}".center(100)
     counter += 1
     end
     puts "Students by cohort: ".center(100)
     
-    cohort_sort = students.sort_by {|hash| hash[:cohort]}
+    cohort_sort = @students.sort_by {|hash| hash[:cohort]}
     cohort_sort.each do |cohort_sort| 
         puts "#{cohort_sort[:cohort]}: #{cohort_sort[:name]}".center(100)
      end
@@ -55,35 +55,43 @@ end
 end
 
 def print_footer(students)
-    if students.count > 0
-    puts "Overall, we have #{students.count} #{students.count == 1 ? "student" : "students"}".center(100)
+    if @students.count > 0
+    puts "Overall, we have #{@students.count} #{@students.count == 1 ? "student" : "students"}".center(100)
     else
     puts " No students yets ".center(100, "~")
 end
 end
 
 def interactive_menu
-    students = []
     loop do
-        # 1. print the menu and ask for the user what to do
+        print_menu
+        process(gets.chomp)  # passing gets.chomp as an arguement for the method
+    end
+end
+
+def print_menu
         puts "1. Input the students"
         puts "2. Show the students"
         puts "9. Exit"
-        # 2. read the input and save it into a variable
-        selection = gets.chomp
-        # 3. do what the user has asked    
-        case selection
-        when "1" then  students = input_students # calling the method input students
-        when "2"
-            print_header
-            print(students)
-            print_footer(students)
-        when "9" then  exit  # this will cause the program to terminate
-        else           puts "I don't know what you meant, try again"
-        end
-    
-    end
 end
+
+def show_students
+        print_header
+        print_students_list(@students)
+        print_footer(@students)
+end
+
+def process(selection)
+        case selection
+          when "1" then input_students # calling the method input students
+          when "2" then show_students
+          when "9" then  exit  # this will cause the program to terminate
+          else           puts "I don't know what you meant, try again"
+        end
+
+end
+    
+
 # nothing happens until we call the methods
 
 interactive_menu
