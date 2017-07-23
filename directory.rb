318365bde80db1f3f611b9a5321492ffd692ad90
David Divilly <d.divilly@gmail.com>
2017-07-23 12:50:51 +0000

    12 loading the csv file

--- directory.rb
+++ directory.rb
@@ -73,6 +73,7 @@ def print_menu
         puts "1. Input the students"
         puts "2. Show the students"
         puts "3. Save the list to students.csv"
+        puts "4. Load the list from students.csv"
         puts "9. Exit"
 end
 
@@ -87,6 +88,7 @@ def process(selection)
           when "1" then input_students # calling the method input students
           when "2" then show_students
           when "3" then save_students
+          when "4" then load_students
           when "9" then  exit  # this will cause the program to terminate
           else           puts "I don't know what you meant, try again"
         end
@@ -99,13 +101,25 @@ def save_students
     # iterate over the array of students
     @students.each do |student|
         student_data = [student[:name], student[:cohort]] # creating an array
-        csv_line = student_data.join(", ")   # and changing it to a string, to be able to puts
+        csv_line = student_data.join(",")   # and changing it to a string, to be able to puts
         file.puts csv_line    # puting cvs_line to the file, rather than the screen
     end
     file.close
 end
-        
+
+def load_students
+    file = File.open("students.csv", "r") # read mode 
+    file.readlines.each do |line|         # readlines # IO (in/output class)
+    name, cohort = line.chomp.split(',')
+        @students << {name: name, cohort: cohort.to_sym}
+    # chomp the trialing new line, "parallel assignment", assigning 2 variables
+    # at the same time. If an array, the first var will get this first value of 
+    # the array, the second var, the second etc.
+    # then puitting into a new Hash
+    end                                    
+    file.close
+
+end
 
 # nothing happens until we call the methods
-
 interactive_menu
